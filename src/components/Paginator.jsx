f757682b648646e1f9161813a889dbcce88f8ecb
vaniya <vaniya.karakasiyan@gmail.com>
2020-06-16 00:32:22 +0300

    minor tweaks [v2]

--- src/components/Paginator.jsx
+++ src/components/Paginator.jsx
@@ -5,18 +5,13 @@ const buttonNames = [`1..10`, `11..20`, `21..30`, `31..40`, `41..50`, `51..60`, 
 const Paginator = ({pagesCount, pageNumber, onPageNumberChange}) => {
   const buildButtonsArray = (pagesCount, pageNumber) => {
     const buttons = [];
+    const basicStyle = {padding: `2px 4px`, outline: `2px solid grey`, cursor: `pointer`, fontSize: `11px`};
+    const activeStyle = {backgroundColor: `grey`, padding: `2px 4px`, outline: `2px solid grey`, cursor: `pointer`, fontSize: `11px`}
 
     for(let i = 0; i < pagesCount; i++) {
       buttons.push(
         <li key={i} style={{display: `inline-block`, margin: `10px`}}>
-          <div 
-            onClick={() => onPageNumberChange(i + 1)}
-            style={
-              (pageNumber === (i + 1))
-              ? {backgroundColor: `grey`, padding: `2px 4px`, outline: `2px solid grey`, cursor: `pointer`, fontSize: `11px`}
-              : {padding: `2px 4px`, outline: `2px solid grey`, cursor: `pointer`, fontSize: `11px`}
-            }
-          >
+          <div onClick={() => onPageNumberChange(i + 1)} style={(pageNumber === (i + 1)) ? activeStyle : basicStyle}>
             {buttonNames[i]}
           </div>
         </li>
@@ -29,7 +24,7 @@ const Paginator = ({pagesCount, pageNumber, onPageNumberChange}) => {
   return (
     <div style={{width: `100%`, display: `flex`, justifyContent: `center`}}>
       <ul style={{listStyleType: `none`, padding: `0`}}>
-      {buildButtonsArray(pagesCount, pageNumber)}
+        {buildButtonsArray(pagesCount, pageNumber)}
       </ul>
     </div> 
   );
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 // import MainPageContainer from './components/MainPageContainer.jsx';
 import RepoPageContainer from './components/RepoPageContainer.jsx';
 
-ReactDOM.render(<RepoPageContainer id={266959801}/>,document.getElementById(`root`));
+ReactDOM.render(<RepoPageContainer id={21737465}/>,document.getElementById(`root`));
 
 //awesome lists 21737465
 //avocode-basics 266959801
